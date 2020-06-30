384558bd2279a0d5a928e89cb4ac52516639df2d
SpearZero <spearzero0@gmail.com>
2020-06-30 18:15:11 +0900

    [RockPaperScissors]
    사용자가 2를 입력하면 보를 반환

--- RockPaperScissors/src/test/java/RCPConverter.java
+++ RockPaperScissors/src/test/java/RCPConverter.java
@@ -1,5 +1,9 @@
 public class RCPConverter {
     public RCP convert(int input) {
+        if(1 == input) return RCP.ROCK;
+        if(2 == input) return RCP.PAPER;
+
+
         return RCP.ROCK;
     }
 }
--- RockPaperScissors/src/test/java/RCPTest.java
+++ RockPaperScissors/src/test/java/RCPTest.java
@@ -5,9 +5,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RCPTest {
 
     @Test
-    public void 사용자가_1을입력하면_주먹을_반환() {
+    public void 사용자가_1을_입력하면_주먹을_반환() {
         RCPConverter converter = new RCPConverter();
         RCP UserRCP = converter.convert(1);
-        assertEquals(RCP.ROCK, UserRCP.ROCK);
+        assertEquals(RCP.ROCK, UserRCP);
+    }
+
+    @Test
+    public void 사용자가_2를_입력하면_보를_반환() {
+        RCPConverter converter = new RCPConverter();
+        RCP UserRCP = converter.convert(2);
+        assertEquals(RCP.PAPER, UserRCP);
     }
 }
